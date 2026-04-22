ff98e28a94d53cd1387225d2fedd5b45b03074ec
753770-netizen <753770@student.dusd.net>
2026-04-22 12:23:44 -0700

    Near completion with 4.3

---
+++ P6Unit4DeLeon/Assets/Scripts/PlayerControlller.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerControlller : MonoBehaviour
+{
+
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject focalPoint;
+    public bool hasPowerup;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+            hasPowerup = true;
+        Destroy(other.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy")&& hasPowerup)
+            Debug.Log("Collided with " + collision.gameObject.name) + " with powerup set to " + hasPowerup);
+    }
+}
